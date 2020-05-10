297d02b076e1169c3e8a72972c71f1d593a5a150
jael.dubey@heig-vd.ch <jael.dubey@heig-vd.ch>
2020-05-10 18:53:23 +0200

    Update lab. Doesn't work anymore

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 ##
 # Student: Jael Dubey & Robel Teklehaimanot
-# Date   : 03.05.2020
+# Date   : 10.05.2020
 # Title  : Lab 3 - Color Mapping
 # File   : main.py
 ##
@@ -30,16 +30,51 @@ def create_geometry(x_pts, altitudes):
         for j in range(LONG_LARG):
             alt = float(altitudes[i][j])
 
+            # https://fr.wikipedia.org/wiki/Coordonn%C3%A9es_sph%C3%A9riques#Conventions
             x = (EARTH_RADIUS + alt) * cos(radians(i * DIST_BETWEEN_POINTS)) * cos(radians(j * DIST_BETWEEN_POINTS))
             y = (EARTH_RADIUS + alt) * cos(radians(i * DIST_BETWEEN_POINTS)) * sin(radians(j * DIST_BETWEEN_POINTS))
             z = (EARTH_RADIUS + alt) * sin(radians(i * DIST_BETWEEN_POINTS))
 
-            x_pts.append((x, y, z))
+            x_pts.append((x, y, -z))
 
 
 def create_topology(pts):
     for i in range(NB_POINTS):
         pts.append((i, i + 1, i - (LONG_LARG - 1), i - LONG_LARG))
+
+
+def is_lake(altitudes, x, y):
+    current_alt = altitudes[x][y]
+
+    if x == 0:  # Côté gauche
+        if y == 0:  # Coin supérieur gauche
+            return altitudes[x + 1][y] == current_alt and altitudes[x][y + 1] == current_alt
+        if y == LONG_LARG - 1:  # Coin inférieur gauche
+            return altitudes[x + 1][y] == current_alt and altitudes[x][y - 1] == current_alt
+        return altitudes[x + 1][y] == current_alt and altitudes[x][y + 1] == current_alt and altitudes[x][
+            y - 1] == current_alt
+
+    if x == LONG_LARG - 1:  # Côté droit
+        if y == 0:  # Coin supérieur droit
+            return altitudes[x - 1][y] == current_alt and altitudes[x][y + 1] == current_alt
+        if y == LONG_LARG - 1:  # Coin inférieur droit
+            return altitudes[x - 1][y] == current_alt and altitudes[x][y - 1] == current_alt
+        return altitudes[x][y - 1] == current_alt and altitudes[x][y + 1] == current_alt and altitudes[x - 1][
+            y] == current_alt
+
+    if y == 0:  # Côté supérieur
+        return altitudes[x - 1][y] == current_alt and altitudes[x + 1][y] == current_alt and altitudes[x][
+            y + 1] == current_alt
+
+    if y == LONG_LARG - 1:  # Côté inférieur
+        return altitudes[x - 1][y] == current_alt and altitudes[x + 1][y] == current_alt and altitudes[x][
+            y - 1] == current_alt
+
+    # Cas général
+    return altitudes[x - 1][y] == current_alt and altitudes[x + 1][y] == current_alt and altitudes[x][
+        y - 1] == current_alt and altitudes[x][y + 1] == current_alt and altitudes[x + 1][y + 1] == current_alt and \
+           altitudes[x + 1][y - 1] == current_alt and altitudes[x - 1][y + 1] == current_alt and altitudes[x - 1][
+               y - 1] == current_alt
 
 
 if __name__ == '__main__':
@@ -51,7 +86,10 @@ if __name__ == '__main__':
     create_geometry(x, altitudes)
     create_topology(pts)
 
-    # Create the VTK datasets
+    # Create the VTK datasets - We use a vtkPolyData mainly because we have already use it in the last lab (cube). We
+    # choose to create square surface, because it was more simple to map with the altitudes files, which is a
+    # 3001x3001 matrix. And finally we heard in your presentation video that the Polydata can be used for this kind of
+    # project.
     geometry = vtk.vtkPoints()
     topology = vtk.vtkCellArray()
     scalars = vtk.vtkFloatArray()
@@ -59,10 +97,11 @@ if __name__ == '__main__':
 
     for i in range(NB_POINTS):
         geometry.InsertPoint(i, x[i])
-        if float(altitudes[int(i / LONG_LARG)][i % LONG_LARG]) != 370:
-            scalars.InsertTuple1(i, float(altitudes[int(i / LONG_LARG)][i % LONG_LARG]))
-        else:
+        # To create the map with the sea level at 370 meters, add the code in comment below
+        if is_lake(altitudes, int(i / LONG_LARG), i % LONG_LARG):  # or float(altitudes[int(i / LONG_LARG)][i % LONG_LARG]) <= 370:
             scalars.InsertTuple1(i, -1)
+        else:
+            scalars.InsertTuple1(i, float(altitudes[int(i / LONG_LARG)][i % LONG_LARG]))
 
     for i in range(1, LONG_LARG):
         for j in range(LONG_LARG - 1):
@@ -94,7 +133,7 @@ if __name__ == '__main__':
         new_colour = ctransfer.GetColor((i * ((5000) / N)))
         lookup_table.SetTableValue(i, *new_colour)
 
-    lookup_table.SetBelowRangeColor(1.0, 0.0, 0.0, 1)
+    lookup_table.SetBelowRangeColor(0.0, 0.0, 0.60, 1)
     lookup_table.UseBelowRangeColorOn()
 
     lookup_table.Build()
